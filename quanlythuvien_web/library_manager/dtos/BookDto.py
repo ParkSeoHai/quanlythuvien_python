337b4cd5448d632d10a35816a92c37e99d533141
Hai Nguyen <anhhai282003@gmail.com>
2024-07-07 09:49:04 +0700

    add column ngay_tao to table books

--- quanlythuvien_web/library_manager/dtos/BookDto.py
+++ quanlythuvien_web/library_manager/dtos/BookDto.py
@@ -1,8 +1,8 @@
 
 class BookDto(object):
     def __init__(self, id_sach = '', name = '', price = 0,
-                 quantity=1, image='', author='', id_category='',
-                is_delete = 0):
+                quantity=1, image='', author='', id_category='',
+                is_delete = 0, ngay_tao=''):
         self.id_sach = id_sach
         self.name = name
         self.price = price
@@ -11,6 +11,7 @@ class BookDto(object):
         self.author = author
         self.is_delete = is_delete
         self.id_category = id_category
+        self.ngay_tao = ngay_tao
 
 
 
